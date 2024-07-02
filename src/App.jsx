b59b25bfa1cf8e1816718b8ffd660641f7074baa
Rbg Reza <rbgreza27@gmail.com>
2024-07-02 14:13:36 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Header from './Header.jsx'
+import Hero from './Hero.jsx'
+import Mid from './Mid.jsx'
+import Review from './Review.jsx'
+import Footer from './Footer.jsx'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />   
+      <Review />  
+      <Mid />  
+      <Footer /> 
+    </>
+  )
+}
+
+export default App
